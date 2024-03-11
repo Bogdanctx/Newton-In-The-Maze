b8671fd34ba972814f905d269bc9784eea298b83
Bogdanctx <bobotrifan09@gmail.com>
2024-03-12 00:02:51 +0200

    Removed template example and created Game, Player, Maze classes.

--- main.cpp
+++ main.cpp
@@ -1,211 +1,121 @@
 #include <iostream>
-#include <array>
-#include <cctype>
-#include <chrono>
-#include <thread>
+#include <vector>
 
-#include <digestpp.hpp>
-#include <rlutil.h>
-#include <random.hpp>
-#include <csv.hpp>
-#include <date.h>
+#include "rlutil.h"
 
-#include <Helper.h>
+class Maze {
+public:
+    Maze(const int &dim = 25) : m_maze(std::vector<std::vector<char>>(dim + 1, std::vector<char>(dim + 1, '#'))) {};
 
-/////////////////////////////////////////////////////////////////////////////////////////
-/// Exemplu de funcții pentru a stoca parole de utilizatori
-/// Folosim biblioteca digestpp pentru a nu stoca parolele în clar
-std::string make_salt() {
-    /// important este ca salt-ul să fie unic, nu contează că nu este aleatoriu
-    /// pentru fiecare user, salt-ul se stochează ca text clar, lângă parola hashed
-    /// Exemplu:
-    /// class User {
-    ///     std::string hashed_password;
-    ///     std::string salt;
-    /// };
-    ///
-    static uint64_t nr = 1u;
-    std::string salt;
-    auto bytes = static_cast<char*>(static_cast<void*>(&nr));
-    for(unsigned i = 0; i < 16; i++) {
-        salt += bytes[i%8];
+    friend std::ostream &operator<<(std::ostream &os, Maze &maze);
+
+    void generate();
+
+private:
+    std::vector<std::vector<char>> m_maze;
+};
+
+std::ostream &operator<<(std::ostream &os, Maze &maze) {
+    for (const auto &i: maze.m_maze) {
+        for (const auto &j: i) {
+            os << j;
+        }
+        os << '\n';
     }
-    ++nr;
-    return salt;
+    return os;
 }
 
-std::string hash_password(const std::string& plain, const std::string& salt) {
-    return digestpp::blake2b(512).set_salt(salt).absorb(plain).hexdigest();
-}
-/////////////////////////////////////////////////////////////////////////////////////////
+void Maze::generate() {
 
-#include <Helper.h>
+}
+
+class Player {
+public:
+    Player(const int &posX, const int &posY) : m_posX(posX), m_posY(posY) {};
+
+    void set_newPosition(const int &key_pressed, const int &maze_dim);
+
+    friend std::ostream &operator<<(std::ostream &os, Player &player);
+
+private:
+    int m_posX, m_posY;
+};
+
+std::ostream &operator<<(std::ostream &os, Player &player) {
+    rlutil::setColor(rlutil::YELLOW);
+
+    gotoxy(player.m_posX, player.m_posY);
+    os << 'A';
+    os.flush();
+    rlutil::setColor(rlutil::WHITE);
+
+    return os;
+}
+
+void Player::set_newPosition(const int &key_pressed, const int &maze_dim) {
+    if (key_pressed == 119 && m_posY - 1 >= 0) { // w
+        --m_posY;
+    } else if (key_pressed == 97 && m_posX - 1 >= 0) { // a
+        --m_posX;
+    } else if (key_pressed == 115 && m_posY + 1 <= maze_dim) { // s
+        ++m_posY;
+    } else if (key_pressed == 100 && m_posX + 1 <= maze_dim) { // d
+        ++m_posX;
+    }
+}
+
+class Game {
+public:
+    Game(const int &maze_size) : m_maze(maze_size) {};
+
+    void run();
+
+private:
+    Maze m_maze;
+    Player m_player{5, 5};
+
+    void handleEvent();
+
+    void render();
+
+    const int keys[4] = {119, 97, 115, 100}; // W, A, S, D
+    bool m_isRunning = true;
+};
+
+void Game::run() {
+    m_maze.generate();
+
+    while (m_isRunning) {
+        render();
+        handleEvent();
+
+        rlutil::msleep(300);
+    }
+}
+
+void Game::render() {
+    rlutil::cls();
+
+    std::cout << m_maze;
+    std::cout << m_player;
+}
+
+void Game::handleEvent() {
+    const int key_pressed = rlutil::getkey();
+
+    m_player.set_newPosition(key_pressed, 25);
+
+    //std::cout<<(char)key<<' '<<key<<'\n';
+}
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+    int maze_dim = 25;
+    std::cout << "Enter maze dimension (default = 25): ";
+    std::cin >> maze_dim;
 
-    std::cin.ignore(); // clear last \n
+    Game game{maze_dim};
 
-    std::cout << "-----------------------------------------------\n";
+    game.run();
 
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu criptare parole (digestpp)                   ///
-    ///////////////////////////////////////////////////////////////////////////
-    std::string plain = "temaOOP12345$";
-    auto salt1 = make_salt();  // salt pt user1
-    auto salt2 = make_salt();  // salt pt user2
-    /// Explicație: deși userii au aceeași parolă, vor avea hash-uri diferite
-    /// De ce am vrea asta? Dacă aflăm hash-ul pt un user, nu vom avea automat hash-urile
-    /// și pentru alți utilizatori care au folosit aceeași parolă
-    /// Alte explicații aici: https://en.wikipedia.org/wiki/Rainbow_table
-    ///
-    std::cout << "Parola hashed pt user1: " << hash_password(plain, salt1) << "\n"
-              << "Parola hashed pt user2: " << hash_password(plain, salt2) << "\n";
-    ///
-    /// Altă variantă pentru parole criptate este cu bcrypt, dar trebuie compilat.
-    /// Un exemplu demo (mai vechi) este aici: https://github.com/zackartz/Bcrypt.cpp
-    ///
-    std::cout << "-----------------------------------------------\n";
-
-    ///////////////////////////////////////////////////////////////////////////
-    ///               Exemplu terminal interactiv (rlutil)                  ///
-    ///////////////////////////////////////////////////////////////////////////
-    rlutil::setConsoleTitle("test");
-    rlutil::saveDefaultColor();
-    rlutil::setColor(rlutil::BLUE);
-    rlutil::cls();
-    int key = rlutil::getkey(); // apel blocant; apelează kbhit și getch
-    switch(std::tolower(key)) {
-        case rlutil::KEY_SPACE:
-            std::cout << "pressed space\n";
-            break;
-        case 'w':
-            std::cout << "pressed w\n";
-            break;
-        case 'a':
-            std::cout << "pressed a\n";
-            break;
-        case 's':
-            std::cout << "pressed s\n";
-            break;
-        case 'd':
-            std::cout << "pressed d\n";
-            break;
-        default:
-            std::cout << "other key (" << key << ")\n";
-            break;
-    }
-    std::cout << "test color text\n";
-    rlutil::resetColor();
-
-    std::cout << "-----------------------------------------------\n";
-
-    ///////////////////////////////////////////////////////////////////////////
-    ///                           Exemplu sleep                             ///
-    ///////////////////////////////////////////////////////////////////////////
-    using namespace std::chrono_literals;
-    std::cout << "begin sleep\n";
-    std::this_thread::sleep_for(600ms);
-    std::cout << "end sleep\n";
-
-    std::cout << "-----------------------------------------------\n";
-
-    ///////////////////////////////////////////////////////////////////////////
-    ///                     Exemplu numere aleatoare                        ///
-    ///////////////////////////////////////////////////////////////////////////
-    using Random = effolkronium::random_static;
-    // Random::seed(42);
-    std::cout << Random::get(1, 1000) << "\n";
-
-    std::cout << "-----------------------------------------------\n";
-
-    ///////////////////////////////////////////////////////////////////////////
-    ///           Exemplu fișier CSV (comma separated value)                ///
-    ///////////////////////////////////////////////////////////////////////////
-    using namespace csv;
-    CSVReader reader{"date.csv"};
-    for (CSVRow& row : reader) {
-        std::cout << "nume: " << row["nume"].get_sv() << "\n";
-        // std::cout << "nume: " << row["nume"].get<>() << "\n";
-    }
-
-    std::cout << "-----------------------------------------------\n";
-
-    ///////////////////////////////////////////////////////////////////////////
-    ///              Exemplu de lucru cu date calendaristice                ///
-    ///////////////////////////////////////////////////////////////////////////
-    using namespace std::chrono;
-    using namespace date;
-    using date::sys_days;
-    using date::days;
-    using date::weeks;
-    using date::months;
-    auto d1 = 2022_y/10/01;
-    auto d2 = 2023_y/05/26;
-
-    auto dp1 = sys_days{d1};
-    auto dp2 = sys_days{d2};
-
-    std::cout << "Anul 2022-2023 are "
-              << duration<float, months::period>(dp2 - dp1).count() << " luni"
-              << " sau "
-              << duration<float, weeks::period>(dp2 - dp1).count() << " săptămâni"
-              << " sau "
-              << duration<float, days::period>(dp2 - dp1).count() << " zile"
-              << ", adică "
-              << floor<months>(dp2 - dp1).count() << " luni, "
-              << floor<weeks>(dp2 - dp1 - floor<months>(dp2 - dp1)).count() << " săptămâni, "
-              << floor<days>(dp2 - dp1 - floor<weeks>(dp2 - dp1)).count() - 1 << " zile."
-              << "\n";
-
-    std::cout << "-----------------------------------------------\n";
     return 0;
 }
