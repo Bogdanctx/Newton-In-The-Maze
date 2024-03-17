7d846a00e95d262cdb25e0296565eebb1372b93c
Bogdanctx <bobotrifan09@gmail.com>
2024-03-17 18:50:01 +0200

    Created random() function for generating random numbers with Mersenne Twister 19937 generator Created and finished Object class Added isInside() function for checking if a new pair of coordinates are inside of the matrix Added createHole() function to remove borders around a pair of coordinates Added setter and getter to check if the player has a bomb Changed some colors Now game is rendered only when an updated occured Added minimum and maximum matrix size

--- main.cpp
+++ main.cpp
@@ -1,18 +1,78 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <chrono>
 
 #include "rlutil.h"
 
+inline int random(const int max_val = INT_MAX, const int min_val = 0) {
+    std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+    return static_cast<int>(rng() % (max_val - min_val) + min_val);
+}
+
+////////////////////// OBJECT CLASS DEFINITION ////////////////////////////
+
+class Object {
+public:
+    Object(const int &row, const int &col, const int &color = rlutil::LIGHTRED) : m_crtRow(row), m_crtCol(col),
+                                                                                  m_color(color) {}
+
+    ~Object() = default;
+
+    friend std::ostream &operator<<(std::ostream &out, Object &object);
+
+    std::pair<int, int> getPosition();
+
+private:
+    int m_crtRow, m_crtCol, m_color;
+};
+
+std::ostream &operator<<(std::ostream &out, Object &object) {
+    rlutil::setColor(object.m_color);
+    gotoxy(object.m_crtCol + 1, object.m_crtRow + 1);
+    out << 'O';
+    rlutil::setColor(rlutil::WHITE);
+    return out;
+}
+
+std::pair<int, int> Object::getPosition() {
+    return std::make_pair(m_crtRow, m_crtCol);
+}
+
+////////////////////////////// END OBJECT CLASS ///////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+////////////////////// MAZE CLASS DEFINITION ////////////////////////////
+
 class Maze {
 public:
-    Maze(const int &dim = 25) : m_maze(std::vector<std::vector<char>>(dim + 1, std::vector<char>(dim + 1, '#'))) {};
+    explicit Maze(const int &dim) : m_maze(std::vector<std::vector<char>>(dim, std::vector<char>(dim, '#'))),
+                                    m_dim(dim) {};
+
+    ~Maze() = default;
 
     friend std::ostream &operator<<(std::ostream &os, Maze &maze);
 
+    bool isPositionAvailable(const int &row, const int &col);
     void generate();
 
+    void createHole(const int &row, const int &col);
+
+    std::vector<std::pair<int, int>> getFreeCells();
+
 private:
+    [[nodiscard]] bool isInside(const int &row, const int &col) const;
     std::vector<std::vector<char>> m_maze;
+    const int m_dim;
 };
 
 std::ostream &operator<<(std::ostream &os, Maze &maze) {
@@ -25,97 +85,350 @@ std::ostream &operator<<(std::ostream &os, Maze &maze) {
     return os;
 }
 
-void Maze::generate() {
-
+bool Maze::isInside(const int &row, const int &col) const {
+    return row >= 0 && col >= 0 && row < m_dim && col < m_dim;
 }
+
+void Maze::createHole(const int &row, const int &col) {
+    const int drdc[8][2] = {{-1, -1},
+                            {-1, 0},
+                            {-1, 1},
+                            {0,  1},
+                            {1,  1},
+                            {1,  0},
+                            {1,  -1},
+                            {0,  -1}};
+
+    for (const auto d: drdc) {
+        const int newRow = row + d[0];
+        const int newCol = col + d[1];
+        if (isInside(newRow, newCol) && m_maze[newRow][newCol] == '#') {
+            m_maze[newRow][newCol] = ' ';
+        }
+    }
+}
+
+std::vector<std::pair<int, int>> Maze::getFreeCells() {
+    std::vector<std::pair<int, int>> result;
+    for (int row = 1; row < m_dim; row++) {
+        for (int col = 0; col < m_dim; col++)
+            if (m_maze[row][col] == ' ') {
+                result.emplace_back(row, col);
+            }
+    }
+    return result;
+}
+
+bool Maze::isPositionAvailable(const int &row, const int &col) {
+    return isInside(row, col) && m_maze[row][col] != '#';
+}
+
+void Maze::generate() {
+    for (int col = 0; col < m_dim; col++) {
+        m_maze[0][col] = ' ';
+    }
+    for (int row = 2; row < m_dim; row += 2) {
+        std::vector<std::pair<int, int>> run;
+        for (int col = 1; col < m_dim; col++) {
+            m_maze[row][col] = ' ';
+            run.emplace_back(row, col);
+
+            bool carve_east = random(101) > 30;
+
+            if (carve_east && col + 1 < m_dim) {
+                m_maze[row][col + 1] = ' ';
+            } else {
+                std::pair<int, int> randomCell = run[random(static_cast<int>(run.size()))];
+                m_maze[randomCell.first - 1][randomCell.second] = ' ';
+                if (randomCell.first + 1 < m_dim) {
+                    m_maze[randomCell.first + 1][randomCell.second] = ' ';
+                }
+                run.erase(run.begin(), run.end());
+                col++;
+            }
+        }
+    }
+}
+
+////////////////////////////// END MAZE CLASS ///////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////// PLAYER CLASS DEFINITION ///////////////////////////////////////
 
 class Player {
 public:
-    Player(const int &posX, const int &posY) : m_posX(posX), m_posY(posY) {};
+    Player() : m_crtRow(0), m_crtCol(0), m_hasBomb(false) {};
 
-    void set_newPosition(const int &key_pressed, const int &maze_dim);
+    ~Player() = default;
+
+    void setHasBomb(bool val);
+
+    [[nodiscard]] bool getHasBomb() const;
+
+    void setNewPosition(const int &newRow, const int &newCol);
+
+    std::pair<int, int> getPosition();
 
     friend std::ostream &operator<<(std::ostream &os, Player &player);
 
 private:
-    int m_posX, m_posY;
+    int m_crtRow, m_crtCol;
+    bool m_hasBomb;
 };
 
 std::ostream &operator<<(std::ostream &os, Player &player) {
-    rlutil::setColor(rlutil::YELLOW);
+    rlutil::setColor(rlutil::BROWN);
 
-    gotoxy(player.m_posX, player.m_posY);
-    os << 'A';
+    gotoxy(player.m_crtCol + 1, player.m_crtRow + 1);
+    os << 'T';
     os.flush();
     rlutil::setColor(rlutil::WHITE);
 
     return os;
 }
 
-void Player::set_newPosition(const int &key_pressed, const int &maze_dim) {
-    if (key_pressed == 119 && m_posY - 1 >= 0) { // w
-        --m_posY;
-    } else if (key_pressed == 97 && m_posX - 1 >= 0) { // a
-        --m_posX;
-    } else if (key_pressed == 115 && m_posY + 1 <= maze_dim) { // s
-        ++m_posY;
-    } else if (key_pressed == 100 && m_posX + 1 <= maze_dim) { // d
-        ++m_posX;
-    }
+void Player::setHasBomb(const bool val) {
+    m_hasBomb = val;
 }
+
+bool Player::getHasBomb() const {
+    return m_hasBomb;
+}
+
+void Player::setNewPosition(const int &newRow, const int &newCol) {
+    m_crtRow = newRow;
+    m_crtCol = newCol;
+}
+
+std::pair<int, int> Player::getPosition() {
+    return std::pair<int, int>{m_crtRow, m_crtCol};
+}
+
+/////////////////////////////////// END PLAYER CLASS /////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////// GAME CLASS DEFINITION //////////////////////////////////////////
 
 class Game {
 public:
-    Game(const int &maze_size) : m_maze(maze_size) {};
+    explicit Game(const int &maze_size) : m_maze(maze_size), bomb(Object{0, random(maze_size - 1, 2), rlutil::BLACK}),
+                                          m_mazeSize(maze_size), m_isRunning(true), m_toggleRender(true) {};
+
+    ~Game() = default;
 
     void run();
 
 private:
-    Maze m_maze;
-    Player m_player{5, 5};
-
-    void handleEvent();
+    void handleEvent(bool &renderFlag);
 
     void render();
 
-    const int keys[4] = {119, 97, 115, 100}; // W, A, S, D
-    bool m_isRunning = true;
+    Maze m_maze;
+    Player m_player;
+    Object bomb;
+    std::vector<Object> objects;
+    std::chrono::system_clock::time_point clock;
+
+    const int m_mazeSize;
+    bool m_isRunning;
+    bool m_toggleRender;
 };
 
 void Game::run() {
+    rlutil::setBackgroundColor(rlutil::DARKGREY);
     m_maze.generate();
+    const std::vector<std::pair<int, int>> mazeFreeCells = m_maze.getFreeCells();
+    const int numberOfRandomObjects = random(10, 3) + 1;
+
+    for (int i = 0; i < numberOfRandomObjects; i++) {
+        const int randIndex = random(static_cast<int>(mazeFreeCells.size()));
+        std::pair<int, int> randomPos = mazeFreeCells[randIndex];
+        Object obj{randomPos.first, randomPos.second};
+        objects.push_back(obj);
+    }
 
     while (m_isRunning) {
-        render();
-        handleEvent();
+        handleEvent(m_toggleRender);
 
-        rlutil::msleep(300);
+        if (m_toggleRender) {
+            render();
+            m_toggleRender = false;
+        }
+
+        if (bomb.getPosition().second == m_mazeSize - 1) {
+            const std::chrono::system_clock::time_point elapsedTime = std::chrono::system_clock::now();
+            const int duration = static_cast<int>(std::chrono::duration_cast<std::chrono::seconds>(
+                    elapsedTime - clock).count());
+
+            if (duration >= 5) {
+                const Object newBomb = Object{0, random(m_mazeSize - 3) + 1, rlutil::BLACK};
+                bomb = newBomb;
+                m_toggleRender = true;
+            }
+        }
+
     }
+
+    rlutil::setBackgroundColor(rlutil::BLACK);
 }
 
 void Game::render() {
     rlutil::cls();
 
     std::cout << m_maze;
+
+    const int objectsRemained = static_cast<int>(objects.size());
+    if (objectsRemained != 0) {
+        gotoxy(1, 1);
+        rlutil::setColor(rlutil::LIGHTRED);
+        std::cout << ">>";
+        rlutil::setColor(rlutil::WHITE);
+    } else {
+        gotoxy(1, 1);
+        rlutil::setColor(rlutil::LIGHTGREEN);
+        std::cout << "<<";
+        rlutil::setColor(rlutil::WHITE);
+    }
+
+    std::cout << bomb;
+    for (Object &obj: objects) {
+        std::cout << obj;
+    }
+
     std::cout << m_player;
 }
 
-void Game::handleEvent() {
-    const int key_pressed = rlutil::getkey();
+void Game::handleEvent(bool &renderFlag) {
+    const int key_pressed = rlutil::nb_getch();
+    std::pair<int, int> playerPosition = m_player.getPosition();
+    const int crtRow = playerPosition.first;
+    const int crtCol = playerPosition.second;
 
-    m_player.set_newPosition(key_pressed, 25);
+    if (objects.empty() && crtRow == 0 && crtCol == 0) {
+        m_isRunning = false;
+        return;
+    }
 
-    //std::cout<<(char)key<<' '<<key<<'\n';
+    switch (key_pressed) {
+        case rlutil::KEY_SPACE: {
+            std::erase_if(objects, [&](Object item) {
+                const std::pair<int, int> objPosition = item.getPosition();
+                return playerPosition == objPosition;
+            });
+
+            std::pair<int, int> bombPosition = bomb.getPosition();
+
+            if (playerPosition == bombPosition && bombPosition.second != m_mazeSize - 1) {
+                m_player.setHasBomb(true);
+                const Object offBomb{0, m_mazeSize - 1, rlutil::LIGHTGREEN};
+                bomb = offBomb;
+
+                clock = std::chrono::system_clock::now();
+            }
+
+            renderFlag = true;
+
+            break;
+        }
+
+        case 102: // f
+        case 70: {// F
+            if (m_player.getHasBomb()) {
+                m_maze.createHole(crtRow, crtCol);
+                m_player.setHasBomb(false);
+            }
+
+            renderFlag = true;
+            break;
+        }
+        case 119: // w
+        case 87: { // W
+            if (m_maze.isPositionAvailable(crtRow - 1, crtCol)) {
+                m_player.setNewPosition(crtRow - 1, crtCol);
+            }
+
+            renderFlag = true;
+            break;
+        }
+        case 97: // a
+        case 65: { // A
+            if (m_maze.isPositionAvailable(crtRow, crtCol - 1)) {
+                m_player.setNewPosition(crtRow, crtCol - 1);
+            }
+
+            renderFlag = true;
+            break;
+        }
+        case 115: // s
+        case 83: { // S
+            if (m_maze.isPositionAvailable(crtRow + 1, crtCol)) {
+                m_player.setNewPosition(crtRow + 1, crtCol);
+            }
+
+            renderFlag = true;
+            break;
+        }
+        case 100: // d
+        case 68: { // D
+            if (m_maze.isPositionAvailable(crtRow, crtCol + 1)) {
+                m_player.setNewPosition(crtRow, crtCol + 1);
+            }
+
+            renderFlag = true;
+            break;
+        }
+        default:
+            break;
+    }
 }
 
+//////////////////////////// END GAME CLASS ////////////////////////////////////////
+
+
 int main() {
-    int maze_dim = 25;
-    std::cout << "Enter maze dimension (default = 25): ";
-    std::cin >> maze_dim;
+    rlutil::setCursorVisibility(false);
 
-    Game game{maze_dim};
+    int dim_maze;
 
+    std::cout << "Enter maze size (default=25, min=10, max=35): ";
+    std::cin >> dim_maze;
+    if (dim_maze < 10) {
+        dim_maze = 10;
+    }
+    if (dim_maze > 35) {
+        dim_maze = 35;
+    }
+
+    Game game{dim_maze};
     game.run();
 
+    rlutil::cls();
+    rlutil::setColor(rlutil::LIGHTGREEN);
+    std::cout << "Newton found all the apples. He may now study the laws of gravity\n";
+
+    system("pause");
     return 0;
 }
